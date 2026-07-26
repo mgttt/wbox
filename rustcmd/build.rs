b68ae5df920b917477c84b432a63483b39e6558c
wjc <wanjochan@gmail.com>
2026-07-26 23:49:05 +0800

    remove migrated rustcmd project

--- rustcmd/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=assets/rustcmd.ico");
-    #[cfg(windows)]
-    winresource::WindowsResource::new()
-        .set_icon("assets/rustcmd.ico")
-        .compile()
-        .expect("failed to embed RustCmd icon");
-}
